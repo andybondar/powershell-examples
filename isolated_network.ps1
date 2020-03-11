5f014b1081c836108f3635b5d42d0644276661d9
Andrii.Bondar <andy.bondar@gmail.com>
2020-03-11 22:16:39 +0200

    Added 'isolated_network.ps1' script

---
+++ isolated_network.ps1
@@ -0,0 +1,15 @@
+﻿# Specify your Interface Alias
+$ifalias = "Ethernet"
+
+# Isolated Network IP and Prefix Length
+$ip = "10.2.2.15"
+$prefix = 24
+
+# Remove default route
+Get-NetRoute | Where-Object {$_.DestinationPrefix -eq "0.0.0.0/0"} | Remove-NetRoute -Confirm:$false
+
+# Remove NAT Network IP 
+Get-NetIPAddress | Where-Object {$_.InterfaceAlias -eq $ifalias -and $_.AddressFamily -eq "IPv4"} | Remove-NetIPAddress -Confirm:$false
+
+# Set up Isolated Network IP
+New-NetIPAddress -InterfaceAlias $ifalias -IPAddress $ip -AddressFamily IPv4 -PrefixLength $prefix
